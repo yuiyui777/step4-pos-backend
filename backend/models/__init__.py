ba37e67334edeeb86e296e56ac3475dc086efbdc
yuiyui777 <yui.likewhat@gmail.com>
2025-10-16 03:18:00 +0900

    Delete backend directory

--- backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from database import Base
-from .product_master import ProductMaster
-from .transaction import Transaction
-from .transaction_detail import TransactionDetail
-
-__all__ = ['Base', 'ProductMaster', 'Transaction', 'TransactionDetail']
-
